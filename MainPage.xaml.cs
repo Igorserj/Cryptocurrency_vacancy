34f86045da8bf28fe8a7e4304dc6dc939bc4950b
Igor Serj <igorserj2000@gmail.com>
2023-01-13 19:52:49 +0200

    MainPageGrid

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,26 +2,32 @@
 using Newtonsoft.Json.Linq;
 using RestSharp;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Cryptocurrency
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    public class Currency
+    {
+        public string Usds { get; set; }
+        public string Names { get; set; }
+    }
+
     public sealed partial class MainPage : Page
     {
+        public List<Currency> Currencies { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
             Client();
         }
+
+
         private void Client()
         {
+            Currencies = new List<Currency>();
             var client = new RestClient("https://api.coincap.io/v2/assets");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
@@ -32,28 +38,18 @@ namespace Cryptocurrency
             JArray items = (JArray)jsonObject["data"];
             int length = items.Count;
 
-            List<TextBlock> textBlocks = new List<TextBlock>();
+            //StringCollection usds = new StringCollection();
             for (int i = 0; i < length; i++)
             {
-                var newJsonArray = jsonObject.SelectTokens("data[" + i + "].priceUsd");
-                var result = JsonConvert.SerializeObject(newJsonArray, Newtonsoft.Json.Formatting.Indented);
+                var priceArray = jsonObject.SelectTokens("data[" + i + "].priceUsd");
+                var result = JsonConvert.SerializeObject(priceArray, Newtonsoft.Json.Formatting.Indented);
 
-                textBlocks.Add(new TextBlock());
-                textBlocks[i].Text = result.ToString();
-                textBlocks[i].Opacity = 1;
-                //SomeText.Text = result.ToString();
-                //textBlock = new TextBlock();
+                var nameArray = jsonObject.SelectTokens("data[" + i + "].id");
+                var result2 = JsonConvert.SerializeObject(nameArray, Newtonsoft.Json.Formatting.Indented);
+                //usds.Add(result.ToString());
+                Currencies.Add(new Currency { Usds = result.ToString(), Names = result2.ToString() });
+
             }
-
-
-            //var jsonArray = JArray.Parse(result);
-            //newJsonArray = jsonArray.SelectTokens("$..bitcoin");
-            //result = JsonConvert.SerializeObject(newJsonArray, Newtonsoft.Json.Formatting.Indented);
-
-            //var data = (JObject)JsonConvert.DeserializeObject(result);
-            //var timeZone = data["priceUsd"].Value<JObject>();
-            //JArray jArr = (JArray)JsonConvert.DeserializeObject(result);
-            //int count = result.Count;
         }
     }
 }
